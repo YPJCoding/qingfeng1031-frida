20577706636d1ef9cf3a1c46df9f59f1aabcd74b
YPJCoding <178675502@qq.com>
2026-06-12 22:29:30 +0800

    deploy: 重启前清理 plugin.log

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,9 @@ echo "=== 同步源码到 ${HOST}:${TARGET} ==="
 scp frida.js frida_config.json frida.config "${HOST}:${TARGET}/"
 scp frida_modules/*.js "${HOST}:${TARGET}/frida_modules/"
 
+echo "=== 清理 plugin.log ==="
+ssh "${HOST}" "rm -f ${TARGET}/plugin.log"
+
 echo "=== 重启 Docker 容器 ==="
 ssh "${HOST}" "docker restart dnf"
 
