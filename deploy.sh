0d8dcd5ce42258029c691b53b42c830b091595ec
YPJCoding <178675502@qq.com>
2026-06-13 08:23:48 +0800

    refactor(C): reorganize into core/api/features/commands subdirs

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,11 @@ TARGET="/data/data/frida"
 echo "=== 同步源码到 ${HOST}:${TARGET} ==="
 
 scp frida.js frida_config.json frida.config "${HOST}:${TARGET}/"
-scp frida_modules/*.js "${HOST}:${TARGET}/frida_modules/"
+scp frida_modules/main.js frida_modules/legacyAliases.js "${HOST}:${TARGET}/frida_modules/"
+scp frida_modules/core/*.js "${HOST}:${TARGET}/frida_modules/core/"
+scp frida_modules/api/*.js "${HOST}:${TARGET}/frida_modules/api/"
+scp frida_modules/features/*.js "${HOST}:${TARGET}/frida_modules/features/"
+scp frida_modules/commands/*.js "${HOST}:${TARGET}/frida_modules/commands/"
 scp data/item_name_list.txt "${HOST}:${TARGET}/data/"
 
 echo "=== 清理 plugin.log ==="
